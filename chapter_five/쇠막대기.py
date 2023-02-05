7381b107da60839e3612639c6a373bf290dc7d99
padonan <bbde1861@gmail.com>
2023-02-05 19:59:51 +0900

    02 05

--- chapter_five/쇠막대기.py
+++ chapter_five/쇠막대기.py
@@ -2,5 +2,16 @@ import sys
 sys.stdin = open("C:\\Study\\algorithm\\input.txt", "r")
 
 a = list(map(str, input()))
+stack = []
+sum = 0
 
-print(a)
+for i in range(len(a)):
+    if a[i] == "(":
+        stack.append(a[i])
+    else:
+        stack.pop()
+        if a[i-1] == "(":
+            sum += len(stack)
+        else:
+            sum += 1
+print(sum)
