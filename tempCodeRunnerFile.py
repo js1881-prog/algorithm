24373324201490395a92ccd3830f849d754ae859
padonan <bbde1861@gmail.com>
2023-04-03 11:56:05 +0900

    04 03

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,16 @@
+arr = [1,1,3,3,0,1,1]
+p1=0
+p2=1
 
+def solution(arr):
+    global p1,p2
+    while(True):
+        if p2 >= len(arr):
+            return arr
+        if arr[p1]==arr[p2]:
+            arr.pop(p1)
+        else:
+            p1 += 1
+            p2 += 1
 
-print(ingredient[1:7])
+print(solution(arr))
