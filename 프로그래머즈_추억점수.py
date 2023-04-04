82cd888ff6c9b35e30c96a5f152bdf8aad7930b2
padonan <bbde1861@gmail.com>
2023-04-05 02:39:29 +0900

    04 05

---
+++ 프로그래머즈_추억점수.py
@@ -0,0 +1,20 @@
+name = ["may", "kein", "kain", "radi"]
+yearning = [5, 10, 1, 3]
+photo = [["may", "kein", "kain", "radi"],
+["may", "kein", "brin", "deny"], ["kon", "kain", "may", "coni"]]
+
+def solution(name, yearning, photo):
+    ls = []
+    res = 0
+    for p in photo:
+        for s in p:
+            if s in name:
+                res += yearning[name.index(s)]
+        ls.append(res)
+        res = 0
+    return ls
+    
+
+solution(name, yearning, photo)
+
+
