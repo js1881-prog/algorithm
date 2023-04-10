7008cf555acee639e3f2474e89f8056edc232d86
padonan <bbde1861@gmail.com>
2023-04-11 00:36:47 +0900

    04 11

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,16 +1,4 @@
-arr = [1,1,3,3,0,1,1]
-p1=0
-p2=1
 
-def solution(arr):
-    global p1,p2
-    while(True):
-        if p2 >= len(arr):
-            return arr
-        if arr[p1]==arr[p2]:
-            arr.pop(p1)
-        else:
-            p1 += 1
-            p2 += 1
-
-print(solution(arr))
+            dQ.append(next)
+            ch[next] = 1
+            dis[next] = dis[now]+1
